baec9afcdbcdabf16876823fa0d13b0977a06477
Abraham Rubin <abrahamgrubin@gmail.com>
2014-07-01 15:35:26 -0300

    Associated users and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
   has_many :comments
+  belongs_to :user
+
+  default_scope { order('creater_at DESC') }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,40 @@
-# db/seeds.rb
-
 require 'faker'
 
-# Create Posts
-50.times do
-  Post.create(
-    title:  Faker::Lorem.sentence,
-    body:   Faker::Lorem.paragraph
-  )
+#Create Users
+5.times do 
+	user = User.new(
+		name: Faker::Name.name,
+		email: Faker::Internet.email,
+		password: Faker::Lorem.characters(10)
+		)
+	user.skip_confirmation!
+	user.save
 end
-posts = Post.all
+users = User.all
 
-# Create Comments
-100.times do
-  Comment.create(
-    post: posts.sample,
-    body: Faker::Lorem.paragraph
-  )
+
+50.times do 
+	Post.create(
+		user: users.sample,
+		title: Faker::Lorem.sentence,
+		body: Faker::Lorem.paragraph
+		)
+end
+posts  = Post.all
+
+100.times do 
+	Comment.create(
+		post: posts.sample,
+		body: Faker::Lorem.paragraph
+		)
 end
 
-puts "Seed finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+User.first.update_attributes(
+	email: 'abrahamgrubin@gmail.com',
+	password: 'Icecream1',
+	)
+
+	puts "Seed finished"
+	puts "#{User.count} users created"
+	puts "#{Post.count} posts created"
+	puts "#{Comment.count} comments created"

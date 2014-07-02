9d9eaa726e2a3ce0ef2cb9763e808e3d1a645454
Abraham Rubin <abrahamgrubin@gmail.com>
2014-07-02 20:58:26 -0300

    Associated and nested topics and posts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,17 @@ require 'faker'
 end
 users = User.all
 
+15.times do 
+	Topic.create(
+		name: Faker::Lorem.sentence,
+		description: Faker::Lorem.paragraph)
+end
+topics = Topic.all
 
 50.times do 
 	Post.create(
 		user: users.sample,
+		topic: topics.sample,
 		title: Faker::Lorem.sentence,
 		body: Faker::Lorem.paragraph
 		)
@@ -29,10 +36,33 @@ posts  = Post.all
 		)
 end
 
-User.first.update_attributes(
-	email: 'abrahamgrubin@gmail.com',
-	password: 'Icecream1',
-	)
+admin = User.new(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+admin.skip_confirmation!
+admin.save
+
+# Create a moderator
+moderator = User.new(
+  name:     'Moderator User',
+  email:    'moderator@example.com', 
+  password: 'helloworld',
+  role:     'moderator'
+)
+moderator.skip_confirmation!
+moderator.save
+
+# Create a member
+member = User.new(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld',
+)
+member.skip_confirmation!
+member.save
 
 	puts "Seed finished"
 	puts "#{User.count} users created"

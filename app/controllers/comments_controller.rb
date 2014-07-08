2f4224a90e372ecbd4db8d984a30fd74e41bc3db
Abraham Rubin <abrahamgrubin@gmail.com>
2014-07-08 16:00:08 -0300

    Destroyed topics, posts and comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,22 @@
 class CommentsController < ApplicationController
   def create
-    
+    redirect_to @comment
   end
+
+  def destroy 
+  	@topic = Topic.find(params[:topic_id])
+  	@post = @topic.posts.find(params[:post_id])
+
+  	@comment = @post.comments.find(params[:id])
+  	authorize @comment
+  	if @comment.destroy
+  		flash[:notice] = "Comment was removed."
+  		redirect_to [@topic, @post]
+  	else
+  		flash[:notice] = "Comment couldn't be deleted. Try again."
+  		redirect_to [@topic, @post]
+  	end
+  end
+
 end
+
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,7 @@ class PostsController < ApplicationController
     authorize @post
     if @post.save
       flash[:notice] = "Post was saved."
-      redirect_to [@topic, @post], notic: "Post was saved successfully."
+      redirect_to [@topic, @post], notice: "Post was saved successfully."
     else 
       flash[:error] = "There was an error saving the post. Please try again."
       render :new
@@ -44,6 +44,21 @@ class PostsController < ApplicationController
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :new
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+
+    title = @post.title
+    authorize @post
+    if @post.destroy
+      flash[:notice] = "\"#{title}\" was deleted successfully "
+      redirect_to @topic
+    else
+      flash[:error] = "There was an error deleting the post"
+      render :show
     end
   end
 

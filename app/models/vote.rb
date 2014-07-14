e97fd0824536a10115c8587e1a3e6a60688ef8a8
Abraham Rubin <abrahamgrubin@gmail.com>
2014-07-14 13:17:01 -0300

    Interlude changes for registration, email permissions and styling

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,6 +5,7 @@ class Vote < ActiveRecord::Base
   validates :value, inclusion: {in: [-1, 1], message: "%{value} is not a valid vote."}
 
   after_save :update_post
+ 
 
   def up_vote?
   	value == 1
